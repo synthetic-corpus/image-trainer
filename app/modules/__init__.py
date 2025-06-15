2b7a54bf0caa1b14662899e1dae831f9cab7245a
Joel G <joel@joelgonzaga.com>
2025-06-15 13:59:35 -0700

    fixed the init file.

--- app/modules/__init__.py
+++ app/modules/__init__.py
@@ -4,7 +4,7 @@ S3 and CDN modules for image trainer application.
 This package provides classes for managing S3 operations and CDN functionality.
 """
 
-from .s3_access import S3Access
-from .cdn import CDN
+from s3_access import S3Access
+from cdn import CDN
 
 __all__ = ['S3Access', 'CDN']
